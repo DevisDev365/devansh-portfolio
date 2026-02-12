5d77345a19288ff0ffe5ead06964b470f927ca46
DevanshKhanna88 <devansh.khanna24@dsb.edu.in>
2026-02-12 12:09:19 +0530

    fix: Restore missing imports in Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Reveal } from './ui/Reveal';
 import { PROJECTS, SKILL_LINKS } from '../constants';
 import { Github, ArrowUpRight, Target, BrainCircuit, CheckCircle2 } from 'lucide-react';
 
